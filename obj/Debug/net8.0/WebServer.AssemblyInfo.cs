c32156a18eaf2a3464965839639d790b1a769b64
roie13 <162023262+roie13@users.noreply.github.com>
2024-05-16 10:36:25 +0300

    added .gitignore

--- obj/Debug/net8.0/WebServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0223f9d4af35e7da1aa93785a29d4f2cf0f415")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
